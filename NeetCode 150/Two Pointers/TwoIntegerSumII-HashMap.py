2e7b7cd59dec36885cd8361b893880cbbc741db2
Ralware <ral.vulns@gmail.com>
2026-08-23 13:29:18 +0530

    Two Integer Sum II

---
+++ NeetCode 150/Two Pointers/TwoIntegerSumII-HashMap.py
@@ -0,0 +1,17 @@
+# Input: Nums = [1,1,3,4], Target = 2
+# Output: [1,2]
+
+def twoSum(Nums,Target):
+    
+    Data = {}
+    
+    for Index in range(len(Nums)):
+        
+        if (Target-Nums[Index]) in Data:
+            return [Data[Target-Nums[Index]]+1,Index+1]
+        
+        Data[Nums[Index]] = Index
+    
+    return []
+
+print(twoSum([1,1,3,4],2))
---
+++ NeetCode 150/Two Pointers/TwoIntegerSumII-TwoPointer.py
@@ -0,0 +1,17 @@
+def twoSum(Nums, Target):
+    
+    Low = 0
+    High = len(Nums)-1
+    
+    while Low < High:
+        
+        Sum = Nums[Low] + Nums[High]
+        
+        if Sum == Target:
+            return [Low+1,High+1]
+        
+        elif Sum < Target:
+            Low+=1
+            
+        else:
+            High-=1
