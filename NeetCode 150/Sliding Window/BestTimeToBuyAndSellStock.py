38f5efc501a7ca703c6d4ccd5eecb51bc752eea6
Ralware <ral.vulns@gmail.com>
2026-08-30 01:04:34 +0530

    BestTimeToBuyAndSellStock

---
+++ NeetCode 150/Sliding Window/BestTimeToBuyAndSellStock.py
@@ -0,0 +1,20 @@
+# Input: Prices = [10,1,5,6,7,1]
+# Output: 6
+
+
+def maxProfit(Prices: List[int]) -> int:
+    
+    Min = Prices[0]
+    MaxDiff = 0
+    for Index in range(len(Prices)):
+        if Prices[Index] < Min:
+            Min = Prices[Index]
+        
+        Diff = Prices[Index] - Min
+        
+        if Diff > MaxDiff:
+            MaxDiff = Diff
+    
+    return MaxDiff
+
+print(maxProfit([10,5,6,7,1]))
