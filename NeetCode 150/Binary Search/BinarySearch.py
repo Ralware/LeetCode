3612718cd032c2e51acdbf9ade0d0418042cf3db
Ralware <ral.vulns@gmail.com>
2026-08-30 13:46:28 +0530

    Binary Search

---
+++ NeetCode 150/Binary Search/BinarySearch.py
@@ -0,0 +1,15 @@
+def search(Nums, Target):
+        Low = 0
+        High = len(Nums) - 1
+
+        while High >= Low:
+            Mid = (Low + High)//2
+
+            if Nums[Mid] > Target:
+                High = Mid - 1
+            elif Nums[Mid] < Target:
+                Low = Mid + 1
+            else:
+                return Mid
+        
+        return -1
