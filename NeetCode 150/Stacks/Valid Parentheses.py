c35e4cf688f6f13daece1a3382906e0042b27a32
Ralware <ral.vulns@gmail.com>
2026-09-03 17:07:28 +0530

    Files Renamed

---
+++ NeetCode 150/Stacks/Valid Parentheses.py
@@ -0,0 +1,27 @@
+def isValid(Brackets):
+
+    Stack = []
+
+    BracketSet = {"{", "[", "("}
+
+    Data = {"}": "{", "]": "[", ")": "("}
+
+    for Bracket in Brackets:
+
+        if Bracket in BracketSet:
+            Stack.append(Bracket)
+
+        else:
+
+            if not Stack:
+                return False
+
+            if Stack[-1] == Data[Bracket]:
+                Stack.pop()
+            else:
+                return False
+
+    return len(Stack) == 0
+    
+    
+print(isValid("[()]"))
